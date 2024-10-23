15b4ea16ee7b6f7dba1a37684ae7ca12645e1a61
ellendias01 <ellendias25@hotmail.com>
2024-10-23 12:34:31 -0300

    23/10 - Home Concluida

--- front-end/src/views/TheLogin.vue
+++ front-end/src/views/TheLogin.vue
@@ -1,23 +1,89 @@
 <template>
-  <section>
-    <h1>Lista de Usuários</h1>
-    <ul>
-      <li v-for="user in users" :key="user.id">{{ user.name }}</li>
-      <li v-for="user in users" :key="user.id">{{ user.email }}</li>
-      <li v-for="user in users" :key="user.id">Usuário: {{ user.master ? "ADM" : "COMUM" }}</li>
-    </ul>
-    <div v-if="error" class="error">{{ error }}</div><br>
-    <hr><br>
-    <div>
-      <label for="email">Email</label><br>
-      <input type="text" v-model="email" name="email" id="email"><br>
-      <label for="password">Senha</label><br>
-      <input type="password" v-model="senha" name="password" id="password">
-      <button @click="validarLogin">Login</button><br>
-      {{ email }}
-      {{ senha }}
+  <div class="body">
+    <div class="esquerda">
+      <img class="img" src="../assets/logo.jpg" alt="logo">
     </div>
-  </section>
+    <div class="direita">
+      <transition name="component-fade" mode="out-in">
+        <component :is="view" key="view">
+          <div v-if="view === 'LoginForm'" class="login-form">
+            <div class="nomeprincipal">
+              <h2>Login <span>AgroTech</span></h2>
+            </div>
+
+            <div class="email">
+              <h3>Email</h3>
+              <input type="email" v-model="email" placeholder="pecuariapro@123.com" class="input-email">
+            </div>
+
+            <div class="senha">
+              <h3>Senha</h3>
+              <input type="password" v-model="senha" placeholder="**********" class="input-senha">
+              <a href="#" @click="trocarParaRecuperarSenha">Esqueceu a senha?</a>
+            </div>
+
+            <div class="botao">
+              <button @click="validarLogin">Login</button>
+            </div>
+
+            <div class="criaConta">
+              <h4>Não tem conta? <a href="#" @click="trocarParaCriarConta">Criar Conta</a></h4>
+            </div>
+          </div>
+
+          <div v-else-if="view === 'CriarContaForm'" class="criar-conta-form">
+            <div class="nomeprincipal">
+              <h2>Conta <span>AgroTech</span></h2>
+            </div>
+
+            <div class="email">
+              <h3>Digite seu email</h3>
+              <input type="email" v-model="email" placeholder="pecuariapro@123.com" class="input-email">
+            </div>
+
+            <div class="senha">
+              <h3>Digite uma senha</h3>
+              <input type="password" v-model="senha" placeholder="**********" class="input-senha">
+            </div>
+
+            <div class="senha">
+              <h3>Confirme a senha</h3>
+              <input type="password" placeholder="**********" class="input-senha">
+            </div>
+
+            <div class="botao">
+              <button>Cadastrar</button>
+            </div>
+
+            <div class="criaConta">
+              <h4>Já tem conta? <a href="#" @click="trocarParaLogin">Login Conta</a></h4>
+            </div>
+          </div>
+
+          <div v-else-if="view === 'RecuperarSenhaForm'" class="recuperar-senha-form">
+            <div class="nomeprincipal">
+              <h2>Recuperar Senha <span>AgroTech</span></h2>
+            </div>
+
+            <div class="email">
+              <h4>Esqueceu sua senha? Não se preocupe! <br> Insira o seu endereço de e-mail abaixo <br>e enviaremos um link para redefinir sua senha. </h4>
+              <h3>Email</h3>
+              <input type="email" v-model="email" placeholder="pecuariapro@123.com" class="input-email">
+            </div>
+            
+            <div class="botao">
+              <button>Enviar</button>
+            </div>
+
+            <div class="criaConta">
+              <h4><a href="#" @click="trocarParaLogin">Voltar para o Login</a></h4>
+            </div>
+          </div>
+
+        </component>
+      </transition>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -31,6 +97,7 @@ export default {
       email: "",
       senha: "",
       error: null,
+      view: 'LoginForm',  // Definindo qual formulário exibir
     }
   },
   mounted() {
@@ -56,11 +123,23 @@ export default {
       } else {
         console.log("Email ou senha incorretos.");
       }
+    },
+
+    trocarParaCriarConta() {
+      this.view = 'CriarContaForm';
+    },
+
+    trocarParaLogin() {
+      this.view = 'LoginForm';
+    },
+
+    trocarParaRecuperarSenha() {
+      this.view = 'RecuperarSenhaForm';
     }
   },
 }
 </script>
 
 <style scoped>
-
+/* Adicione seus estilos aqui */
 </style>
