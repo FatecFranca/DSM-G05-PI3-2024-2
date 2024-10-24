b29e9afd1e1841dce5fb5c599da9b204811c4ca9
habbiner <habbiner.andrade@gmail.com>
2024-10-23 21:51:19 -0300

    feat: new views and new routes

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,6 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import TheLogin from '../views/TheLogin.vue';
-import TheHome from '../views/TheHome.vue';
+import TheLogin from '@/views/TheLogin.vue';
+import TheHome from '@/views/TheHome.vue';
+import TheAnimal from '@/views/TheAnimal.vue';
+import TheAnimals from '@/views/TheAnimals.vue';
+import TheFarm from '@/views/TheFarm.vue';
+import TheInfos from '@/views/TheInfos.vue';
+import TheUsers from '@/views/TheUsers.vue';
 
 const routes = [
   {
@@ -12,6 +17,31 @@ const routes = [
     path: '/home',
     name: 'Home',
     component: TheHome,
+  },
+  {
+    path: '/animal',
+    name: 'Animal',
+    component: TheAnimal,
+  },
+  {
+    path: '/animais',
+    name: 'Animals',
+    component: TheAnimals,
+  },
+  {
+    path: '/fazenda',
+    name: 'Farm',
+    component: TheFarm,
+  },
+  {
+    path: '/informacoes',
+    name: 'Infos',
+    component: TheInfos,
+  },
+  {
+    path: '/usuarios',
+    name: 'Users',
+    component: TheUsers,
   }
 ];
 
---
+++ front-end/src/views/TheFarm.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Bem-vindo à Tela Fazenda!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TheFarm',
+};
+</script>
+
+<style scoped></style>
