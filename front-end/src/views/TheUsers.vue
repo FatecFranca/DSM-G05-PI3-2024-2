d0f7275c94e12321f2ca38c691b7999dd29329b8
GabrielFillip <gabrileux@gmail.com>
2024-10-26 10:39:41 -0300

    "Tela usuarios Alt1"

--- front-end/src/views/TheUsers.vue
+++ front-end/src/views/TheUsers.vue
@@ -1,6 +1,43 @@
 <template>
-    <div>
-        <h1>Bem-vindo à Tela de Usuários!</h1>
+    <div class=""> 
+
+    <div class='h-56  w-screen bg-blue-800' b> <!--Div Superior-->
+        <div class="justify-items-center">
+        <h class='text-4xl center bg-white mx-28 '>Usuários</h>
+        <p class='mt-6'>Selecione o que você deseja cadastrar ou atualizar</p>
+    </div>
+    
+    <div class="flex bg-amber-400 mt-20 ml-10 font-bold"> <!--Div engloba Barra filtros e botao cadastrar-->
+    <div class="flex bg-lime-600  items-center"> <!--Div Barra filtros-->
+        <div class=" bg-pink-400  border border-gray-400"> <img class=" mx-7 my-3.5" src="../assets/imgs/filter.png" alt="logo"> </div>
+        <div class=" bg-pink-400  border border-gray-400"> <p class="mx-7 my-3.5">Filtro</p> </div>
+        <div class="flex bg-pink-400  border border-gray-400"> <p class="mx-7 my-3.5">Data</p> <img class=" mx-7 my-3.5" src="../assets/imgs/arrow.png" alt="logo"> </div>
+        <div class="flex bg-pink-400  border border-gray-400"> <p class="mx-7 my-3.5">Status</p> <img class=" mx-7 my-3.5" src="../assets/imgs/arrow.png" alt="logo"></div>
+        <div class="flex bg-pink-400  border border-gray-400"> <img class=" mx-7 my-3.5" src="../assets/imgs/arrow.png" alt="logo"> <p class="mx-7 my-3.5">Limpar Filtro</p>  </div>
+        <div class="flex bg-pink-400  border border-gray-400"> <p class="mx-7 my-3.5">Ordenar Por</p> </div>
+    </div>
+        
+            <button> + CADASTRAR USUÁRIO</button>
+        
+    </div>
+        <div>
+            </div>
+        </div>
+        
+
+    <div> <!--Div Inferior-->
+        <div> <!--Div quadro-->
+            
+        <div> <!--Div Lista Topicos-->
+        </div>
+        
+        <div></div> 
+        <div></div>
+        <div></div>
+        <div></div>
+        </div>
+    </div>
+
     </div>
 </template>
 
