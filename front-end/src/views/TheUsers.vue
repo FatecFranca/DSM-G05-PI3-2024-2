4079c9a30a390cdaa3db7a54a3b96d104ecae5e2
GabrielFillip <gabrileux@gmail.com>
2024-10-27 15:17:03 -0300

    The Users Alt5

--- front-end/src/views/TheUsers.vue
+++ front-end/src/views/TheUsers.vue
@@ -1,15 +1,14 @@
 <template>
     <div class="flex-grow-1 bg-[url('@/assets/imgs/BgColor.png')] flex h-screen bg-cover">
+        <TheSidebar :isSidebarVisible="isSidebarVisible" @toggle-sidebar="toggleSidebar" />
+        <i class="fa fa-bars fa-2x cursor-pointer p-2" aria-hidden="true" @click="toggleSidebar"></i>
 
-        
-        <div class="bg-black h-full w-[74px]"></div> <!--Simulação Barra lateral-->
-            
-        <div class="flex-grow bg-pink-300">
+        <div class="flex-grow bg-pink-300 -ml-1">
         <div class='h-56 bg-orange-400 '> <!-- Div Superior -->
             
     <div class="justify-items-center mt-0">
         <h class='text-[2.0rem] center'>Usuários</h>
-        <hr class='w-[14rem] mt-2 -mb-3 border-black border-1.5'>
+        <hr class='w-[14rem] mt-2 -mb-3 border-black border-1.5'>x
         <p class='mt-6'>Selecione o que você deseja cadastrar ou atualizar</p>
     </div>
 
@@ -20,36 +19,36 @@
 
             <!-- Icone Filtro -->
             <div class="border rounded-l-lg w-16 h-[3.35rem] border-gray-300 flex justify-center items-center">
-                <img class="w-6 h-[1.57vw]" src="../assets/imgs/filter.png" alt="logo">
+                <img class="w-6 h-[1.57em]" src="../assets/imgs/filter.png" alt="logo">
             </div>
 
             <!-- Filtro -->
             <div class="border border-gray-300 w-28 h-[3.35rem] flex justify-center items-center">
-                <p class="text-[0.87vw]">Filtro</p>
+                <p class="text-[0.87em]">Filtro</p>
             </div>
 
             <!-- Data -->
             <div class="border border-gray-300 w-32 h-[3.35rem] flex justify-center items-center">
-                <p class="text-[0.87vw]">Data</p>
+                <p class="text-[0.87em]">Data</p>
                 <img class="ml-8" src="../assets/imgs/arrow.png" alt="logo">
             </div>
 
             <!-- Ordenar por -->
             <div class="border border-gray-300 w-40 h-[3.35rem] flex justify-center items-center">
-                <p class="text-[0.87vw]">Ordenar Por</p>
+                <p class="text-[0.87em]">Ordenar Por</p>
                 <img class="ml-8" src="../assets/imgs/arrow.png" alt="logo">
             </div>
 
             <!-- Status -->
             <div class="border border-gray-300 w-44 h-[3.35rem] flex justify-center items-center">
-                <p class="text-[0.87vw]">Status</p>
+                <p class="text-[0.87em]">Status</p>
                 <img class='ml-16' src="../assets/imgs/arrow.png" alt="logo">
             </div>
 
             <!-- Limpar Filtro -->
             <div class="rounded-r-lg border border-gray-300 w-40 h-[3.35rem] flex justify-center items-center">
                 <img class="h-4 w-4 mr-2" src="../assets/imgs/back.png" alt="logo">
-                <p class="text-[0.87vw] text-red-600">Limpar Filtro</p>
+                <p class="text-[0.87em] text-red-600">Limpar Filtro</p>
             </div>
         </div>
 
@@ -91,15 +90,16 @@ export default {
         TheSidebar,
     },
     data() {
-        return {
-            isSidebarVisible: true,
-        };
+    return {
+      isSidebarVisible: false,
+      rangeValue: 30,
+    };
+  },
+  methods: {
+    toggleSidebar() {
+      this.isSidebarVisible = !this.isSidebarVisible;
     },
-    methods: {
-        toggleSidebar() {
-            this.isSidebarVisible = !this.isSidebarVisible;
-        },
-    },
+  },
 };
 </script>
 
